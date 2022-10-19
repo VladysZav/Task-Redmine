cb1733551ce9159084ade0b21b2fae5cab73833e
Vladyslav <108288355+VladysZav@users.noreply.github.com>
2022-10-19 10:05:53 +0300

    Update loginPage.js

--- tests/Pages/loginPage.js
+++ tests/Pages/loginPage.js
@@ -1,12 +1,10 @@
 const { expect } = require('@playwright/test');
 
 exports.LoginPage = class LoginPage{
-
     constructor(page) {
         this.page = page;
         this.loginButton = page.locator('[type="submit"]');
     }
-
     async submitButton() {
         await this.loginButton.click();
     }
