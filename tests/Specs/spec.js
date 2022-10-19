deee2706007b16153b2165b95c6bcf5c71e55460
Vladyslav <108288355+VladysZav@users.noreply.github.com>
2022-10-19 10:15:48 +0300

    Update spec.js

--- tests/Specs/spec.js
+++ tests/Specs/spec.js
@@ -2,53 +2,12 @@
 const {MainPage} = require ('../Pages/mainPage');
 const { test, expect } = require('@playwright/test');
 const {RegistrationPage} = require('../Pages/registrationPage')
-const {LoginPage} = require('../Pages/loginPage')
 
-test('TestCase_1', async ({page}) => {
-    
+test('Registration with valid data', async ({page}) => {
     const mainP = new MainPage(page);
     await mainP.goToMainPage();
     await mainP.registration();
-
     const registrationP = new RegistrationPage(page);
     await registrationP.enterData();
     await registrationP.subButton();
 }); 
-
-test('TestCase_2', async ({page}) => {
-
-    const mainP = new MainPage(page);
-    await mainP.goToMainPage();
-    await mainP.registration();
-
-    const registrationP = new RegistrationPage(page);
-    await registrationP.subButton();
-});
-
-test('TestCase_3', async ({page}) => {
-
-    const mainP = new MainPage(page);
-    await mainP.goToMainPage();
-    await mainP.registration();
-
-    const registrationP = new RegistrationPage(page);
-    await registrationP.enterLargeData();
-    await registrationP.subButton();
-});
-
-test('TestCase_4', async ({page}) => {
-
-    const mainP = new MainPage(page);
-    await mainP.goToMainPage();
-    await mainP.logining();
-
-    const loginP = new LoginPage(page);
-    await loginP.submitButton();
-});
-
-test('TestCase_5', async ({page}) => {
-
-    const mainP = new MainPage(page);
-    await mainP.goToMainPage();
-    await mainP.enterTextInSearchBar('example');
-});
