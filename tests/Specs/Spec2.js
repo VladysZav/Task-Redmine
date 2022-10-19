14f5fab4e4bfff2b5c4de5c1ab4daaf7981483e4
Vladyslav <108288355+VladysZav@users.noreply.github.com>
2022-10-19 10:11:26 +0300

    Update and rename Test2.js to Spec2.js

--- tests/Specs/Spec2.js
+++ tests/Specs/Spec2.js
@@ -3,7 +3,6 @@ const { test, expect } = require('@playwright/test');
 const {RegistrationPage} = require('../Pages/registrationPage')
 
 test('Registration with empty lines', async ({page}) => {
-
     const mainP = new MainPage(page);
     await mainP.goToMainPage();
     await mainP.registration();
