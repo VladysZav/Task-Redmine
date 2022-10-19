6cf41790b8df0efe0a4fd5689648fcc16d6f5db4
Vladyslav <108288355+VladysZav@users.noreply.github.com>
2022-10-19 10:05:32 +0300

    Update mainPage.js

--- tests/Pages/mainPage.js
+++ tests/Pages/mainPage.js
@@ -1,7 +1,6 @@
 const { expect } = require('@playwright/test');
 
 exports.MainPage = class MainPage {
-
     constructor(page) {
         this.page = page;
         this.registrationButton = page.locator('[href="/account/register"]');
@@ -9,25 +8,20 @@ exports.MainPage = class MainPage {
         this.inputSearchBar = page.locator('[accesskey="f"]');
         this.registrationForm = page.locator('#new_user');
     }
-
     async goToMainPage() {
         await this.page.goto('https://www.redmine.org/');
     }
-
     async registration(){
         await this.registrationButton.click();
         await this.page.waitForLoadState();
     }
-
     async  logining(){
         await this.loginButton.click();
         await this.page.waitForLoadState();
     }
-
     async enterTextInSearchBar(searchText){
         await this.inputSearchBar.fill(searchText);
         await this.inputSearchBar.press('Enter');
     }
-
 }
     
