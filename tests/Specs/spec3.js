1dcdc2295107dc7fb7231748552dc99ea06f424f
Vladyslav <108288355+VladysZav@users.noreply.github.com>
2022-10-19 10:14:15 +0300

    Update spec3.js

--- tests/Specs/spec3.js
+++ tests/Specs/spec3.js
@@ -2,7 +2,7 @@ const {MainPage} = require ('../Pages/mainPage');
 const { test, expect } = require('@playwright/test');
 const {RegistrationPage} = require('../Pages/registrationPage')
 
-test('TestCase_3', async ({page}) => {
+test('Registration with too long data input', async ({page}) => {
     const mainP = new MainPage(page);
     await mainP.goToMainPage();
     await mainP.registration();
